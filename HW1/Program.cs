2e1bbf2bd99b1c3f051149a0cd1cd10be48271ba
Mutniyh <wrobotw@mail.ru>
2023-01-26 17:31:23 +0300

    код программы на нахождение наибольшего числа из двух данных чисел без стороннего ввода даных

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int f = 3;
+int b = -4;
+int max = 0;
+if (f > b)
+{
+    max = f;
+}
+else
+{
+    max = b;
+}
+
+Console.WriteLine(max);
