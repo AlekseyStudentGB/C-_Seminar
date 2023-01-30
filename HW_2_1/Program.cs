5db4b062f9ff230daf6be7ab53e1c4791c1da552
Mutniyh <wrobotw@mail.ru>
2023-01-30 12:46:19 +0300

    Второе решение

--- HW_2_1/Program.cs
+++ HW_2_1/Program.cs
@@ -5,14 +5,15 @@
 918 -> 1 */
 
 
-int Getnumber(string message)
+int Getnumber(int number)
 {
-Console.Write(message);
-int number = Convert.ToInt32(Console.ReadLine());
+//Console.Write(message);
+//int number = Convert.ToInt32(Console.ReadLine());
 int number1 = number / 10;
 int number2 = number / 100;
-//int result = number1 - (number2 * 10);
-return (number1 - (number2 * 10)); 
+int result = number1 - (number2 * 10);
+return result; 
 }
-int result = Getnumber("число");
-Console.Write($"xbckj {result}");
+Console.Write("Введите трехзначное число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Результат преобразования -> {Getnumber(number)}");
