197d509bb628a659725cc22c1969e8540c59c1ae
Mutniyh <wrobotw@mail.ru>
2023-01-30 12:28:31 +0300

    первое решение

--- HW_2_1/Program.cs
+++ HW_2_1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+
+int Getnumber(string message)
+{
+Console.Write(message);
+int number = Convert.ToInt32(Console.ReadLine());
+int number1 = number / 10;
+int number2 = number / 100;
+//int result = number1 - (number2 * 10);
+return (number1 - (number2 * 10)); 
+}
+int result = Getnumber("число");
+Console.Write($"xbckj {result}");
