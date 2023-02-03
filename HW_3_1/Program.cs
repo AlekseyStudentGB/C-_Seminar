a43624c76d3e7418601ed719c25fcf80c5be045f
Mutniyh <wrobotw@mail.ru>
2023-02-03 14:40:33 +0300

    Решение  HW  1 к семинару 3

--- HW_3_1/Program.cs
+++ HW_3_1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+*/
+
+int GetNumber (string message)
+{
+    int result = 0;
+    Console.Write(message);
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result) && result > 9999 && result <100000)
+        break;
+
+        else Console.WriteLine("ВВЕДИТЕ КОРРЕКТНОЕ ЧИСЛО");
+
+
+    }
+    return result;
+}
+
+void YesNo(int number)
+{
+if (number / 10000 == number % 10 && number / 1000 % 10 == (number / 10) % 10) 
+{
+    Console.WriteLine("Да!");
+}
+else Console.WriteLine("Нет!");    
+}
+
+int number = GetNumber("Введите число -> ");
+YesNo(number);
