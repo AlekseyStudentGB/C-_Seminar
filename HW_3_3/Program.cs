c15d41363662c1fe0f4a26a9e014f48b5c1fd9cc
Mutniyh <wrobotw@mail.ru>
2023-02-06 14:17:41 +0300

    Вариант HW 3 к семинару 3

--- HW_3_3/Program.cs
+++ HW_3_3/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int Number(string msg)
+{
+int n = 0;
+Console.WriteLine(msg);
+while(true)
+{
+    if(int.TryParse(Console.ReadLine(), out n) && n > 0)
+    {
+    break;
+    }
+    else Console.WriteLine("-----");      
+}
+return n; 
+}
+
+void GetArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = i + 1;  
+}
+
+
+}
+
+void PrintT(int[] array)
+{ 
+int result = 0;
+
+for(int i = 0; i < array.Length; i++)
+{
+    result =Convert.ToInt32(Math.Pow(array[i],3));
+    Console.WriteLine($"{array[i]} в степени 3 -> {result}");
+}
+
+
+
+}
+
+int n = Number("введите число");
+int[] array = new int [n];
+GetArray(array);
+PrintT(array);
