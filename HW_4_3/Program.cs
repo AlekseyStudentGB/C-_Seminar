154a6f14f6695ed92ba8e09c1b56669d1df4c5e5
Mutniyh <wrobotw@mail.ru>
2023-02-09 09:39:29 +0300

    Решение HW 4_3

--- HW_4_3/Program.cs
+++ HW_4_3/Program.cs
@@ -1,4 +1,33 @@
-﻿int[] array = { 1, 2, 3 };
+﻿
+
+void GetArray(int[] array)
+{
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 8);
+    }
+}
 
 
-Console.Write($"{array[2]},");
+
+int[] array = new int[8];
+GetArray(array);
+int size = array.Length;
+int i = 0;
+while (i < size)
+{
+    if (i < size - 1)
+    {
+        Console.Write($"{array[i]}, ");
+        i++;
+    }
+    else
+    {
+        Console.Write(array[i]);
+        break;
+    }
+}
+return i;
+
