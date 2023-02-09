3a1630da4a70f311e47004cba5a7f33f016ae1d9
Mutniyh <wrobotw@mail.ru>
2023-02-09 09:08:41 +0300

    Решение HW 4_2

--- HW_4_2/Program.cs
+++ HW_4_2/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+
+
+int Getsum(int num)
+{
+    int i = 0;
+    int sum = 0;
+    int numwork = num;
+    while (numwork > 0)
+        if (numwork > 0)
+        {
+            sum += numwork % 10;
+            numwork = numwork / 10;
+            i++;
+        }
+        else break;
+    return sum;
+}
+int Numb(string msg)
+{
+    int num = 0;
+    Console.Write(msg);
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out num))
+        {
+            break;
+        }
+        else Console.WriteLine("Elese");
+
+    }
+    return num;
+}
+
+int num = Numb("Число дайте пж -> ");
+int size = Getsum(num);
+
+Console.WriteLine(size);
