7940ced1294e5e855083a271263d921c19383da5
Mutniyh <wrobotw@mail.ru>
2023-02-15 12:16:03 +0300

    Решение задачи 2 к семенару 5

--- HW_5_2/Program.cs
+++ HW_5_2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+void GetArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-99, 100);
+        Console.WriteLine(array[i]);
+    }
+}
+
+int GetSumm(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0) sum = sum + array[i];
+    }
+    return sum;
+}
+
+int[] array = new int[4];
+GetArray(array);
+Console.WriteLine();
+Console.WriteLine(GetSumm(array));
