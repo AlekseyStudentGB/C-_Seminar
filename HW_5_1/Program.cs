391e7e5c23629743761311e9484a69a1d8836cee
Mutniyh <wrobotw@mail.ru>
2023-02-15 11:55:54 +0300

    Решение задачи 1 к семинару 5

--- HW_5_1/Program.cs
+++ HW_5_1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+void GetArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+        Console.WriteLine(array[i]);
+    }
+}
+
+int GetCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count ++; 
+    }
+    return count;
+}
+
+int[] array = new int[4];
+GetArray(array);
+Console.WriteLine();
+Console.WriteLine(GetCount(array));
+
