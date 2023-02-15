b085a88c6df5ed37a7d99e8ff66322dffd8fedce
Mutniyh <wrobotw@mail.ru>
2023-02-15 13:28:00 +0300

    Решение задачи 3 к семинару 5

--- HW_5_3/Program.cs
+++ HW_5_3/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76
+*/
+
+
+void GetArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 100);
+        Console.WriteLine(array[i]);
+    }
+
+}
+
+(int, int) GetMaxMin(int[] array)
+{
+    int max = array[0];
+    int min = array[0];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max) max = array[i];
+        if (array[i] < min) min = array[i];
+    }
+    return (max, min);
+}
+
+
+int[] array = new int[4];
+GetArray(array);
+(int max, int min) = GetMaxMin(array);
+Console.WriteLine($"Max({max}) - Min({min}) = {max-min}");
