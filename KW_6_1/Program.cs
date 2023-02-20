6140d8920bb5bc44576412d9b1eea9f56674f6ec
Mutniyh <wrobotw@mail.ru>
2023-02-20 09:49:22 +0300

    Work

--- KW_6_1/Program.cs
+++ KW_6_1/Program.cs
@@ -114,7 +114,7 @@ m = 3, n = 4.
 1 -3,3 8 -9,9
 
 8 7,8 -7,1 9
-*/
+*//*
 
 double[,] InitMatrix(int m, int n)
 {
@@ -157,8 +157,95 @@ int GetNumber(string msg)
     return result;
 }
 
+void GetPosishen(double[,] matr)
+{
+int i =GetNumber("строка = ")-1;
+int j =GetNumber("столбец = ")-1;
+if (i <= matr.GetLength(0) &&  j <=  matr.GetLength(1)) Console.WriteLine (matr[i,j]);
+else Console.WriteLine("Такого числа нет"); 
+}
 
 int m =GetNumber("M = ");
 int n =GetNumber("N = ");
 double[,] matrix = InitMatrix(m, n);
 PrintMatrix(matrix);
+GetPosishen(matrix);
+*/
+int[,] InitMatrix(int m, int n)
+{
+    int[,] array = new int[m, n];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+
+    return array;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int GetNumber(string msg)
+{
+    Console.Write(msg);
+    int result = 0;
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result)) break;
+        else Console.WriteLine("НЕ КОРРЕКТНОЕ ЗНАЧЕНИЕ");
+    }
+    return result;
+}
+
+void GetResult(int[,] matr, int m, int n, double[] arr)
+{
+
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[j] += matr[i, j];
+        }
+    }
+    for (int ii = 0; ii < m; ii++)
+    {
+        arr[ii]= arr[ii] / m;
+    }
+}
+
+
+void PrintArr(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i].ToString("F1")}/");
+
+    }
+}
+
+
+
+
+int m = GetNumber("M = ");
+int n = GetNumber("N = ");
+int[,] matrix = InitMatrix(m, n);
+PrintMatrix(matrix);
+double[] arr = new double[m];
+GetResult(matrix, m, n, arr);
+PrintArr(arr);
