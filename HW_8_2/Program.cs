1f3038bfa3e134b632e4e5d8f5915bba55ac94f6
Mutniyh <wrobotw@mail.ru>
2023-02-26 14:13:17 +0300

    Решение задачи 3 к семинару 8

--- HW_8_2/Program.cs
+++ HW_8_2/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+void Initmatr(int[,] matr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rnd.Next(0, 10);
+
+        }
+    }
+}
+
+void PrintMatr(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine("________");
+}
+
+void MaxRow(int[,] matr)
+{
+    int minSum = 100;
+    int positonMinSum = 0;
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+
+        int sumRow = 0;
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            sumRow = sumRow + matr[i, j];
+        }
+        if (minSum > sumRow)
+        {
+            minSum = sumRow;
+            positonMinSum = i;
+        }
+
+    }
+    Console.WriteLine(positonMinSum + 1);
+}
+
+
+
+
+
+
+int[,] matr = new int[5, 4];
+Initmatr(matr);
+PrintMatr(matr);
+MaxRow(matr);
--- HW_8_3/Program.cs
+++ HW_8_3/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+
+void GetMatrResult(int[,] matr1, int[,] matr2, int[,] matrResult)
+{
+    for (int i = 0; i < matrResult.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrResult.GetLength(1); j++)
+        {
+            matrResult[i, j] = GetResult(matr1, matr2, i, j);
+            Console.Write($"{matrResult[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int GetResult(int[,] matr1, int[,] matr2, int i, int j)
+{
+    int[] arr = new int[2];
+    int summ = 0;
+    for (int k = 0; k < matr1.GetLength(0); k++)
+    {
+        arr[k] = matr1[i, k] * matr2[k, j];
+    }
+    for (int l = 0; l < matr1.GetLength(0); l++)
+    {
+        summ = summ + arr[l];
+    }
+    //Console.WriteLine(summ);
+    return summ;
+}
+
+int[,] matr1 = new int[,]
+{
+{2,4},
+{3,2},
+};
+int[,] matr2 = new int[,]
+{
+{3,4},
+{3,3},
+};
+
+int[,] matrResult = new int[matr1.GetLength(0), matr1.GetLength(1)];
+GetMatrResult(matr1, matr2, matrResult);
+
+
