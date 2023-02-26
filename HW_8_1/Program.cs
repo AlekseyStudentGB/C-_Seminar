6113ad9fefa09b8f9f84b1af7d7695df258f03db
Mutniyh <wrobotw@mail.ru>
2023-02-26 14:05:36 +0300

    Решение задачи 4 к семинару 8

--- HW_8_1/Program.cs
+++ HW_8_1/Program.cs
@@ -299,7 +299,7 @@ Initmatr(matr);
 PrintMatr(matr);
 MaxRow(matr);
 *///////////////////////
-/*
+
 void Initmatr(int[,,] matr)
 {int indtxRnd = 1;
     Random rnd = new Random();
@@ -335,50 +335,6 @@ void PrintMatr(int[,,] matr)
 int[,,] matr = new int[2, 2, 2];
 Initmatr(matr);
 PrintMatr( matr);
-*/
-void Initmatr(int[,] matr, int n)
-{
-    int num = 1;
-    for (int i = 0; i < n / 2; i++)
-    {
-        for (int j = i; j < n - i; j++)
-        {
-            
-            matr[i, j] = num++;
-        }
-        for (int k = i + 1; k < n - i; k++)
-        {
-            
-            matr[k, n - i-1] = num++;
-        }
-        for (int j = n-i-2; j >=i; j--)
-        {
-            
-            matr[n-1-i, j] = num++;
-        }
-         for (int k = n-2-i; k > i ; k--)
-        {
-            
-            matr[k,i] = num++;
-        }
-    }
-}
-void PrintMatr(int[,] matr)
-{
-    for (int i = 0; i < matr.GetLength(0); i++)
-    {
-        for (int j = 0; j < matr.GetLength(1); j++)
-        {
-            Console.Write($"{matr[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine("________");
 
-}
-
-int[,] matr = new int[4, 4];
-Initmatr(matr, 4);
-PrintMatr(matr);
 
 
--- HW_8_4/Program.cs
+++ HW_8_4/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)
+*/
+
+void Initmatr(int[,,] matr)
+{int indtxRnd = 1;
+    Random rnd = new Random();
+    for (int k = 0; k < matr.GetLength(0); k++)
+    {
+        for (int i = 0; i < matr.GetLength(1); i++)
+        {
+            for (int j = 0; j < matr.GetLength(2); j++)
+            {
+                matr[k, i, j] = rnd.Next(indtxRnd, indtxRnd +5);
+                indtxRnd += 5;
+
+            }
+        }
+    }
+}
+
+void PrintMatr(int[,,] matr)
+{
+    for (int k = 0; k < matr.GetLength(0); k++)
+    {
+        for (int i = 0; i < matr.GetLength(1); i++)
+        {
+            for (int j = 0; j < matr.GetLength(2); j++)
+            {
+                Console.Write($"{matr[k, i, j]} ({k},{i},{j}) ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("__________________");
+    }
+}
+
+int[,,] matr = new int[2, 2, 2];
+Initmatr(matr);
+PrintMatr( matr);
+
