506b4c927e5e1ac95802da44cf0d440465985f70
Mutniyh <wrobotw@mail.ru>
2023-02-27 09:44:54 +0300

    Решение задачи 1 к семинару 9

--- HW_9_1/Program.cs
+++ HW_9_1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 4; N = 8 -> "4, 6, 7, 8"
+*/
+
+int GetNumber(string msg)
+{
+    Console.Write(msg);
+    int result = 0;
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result)) break;
+        else Console.WriteLine("НЕ КОРРЕКТНОЕ ЗНАЧЕНИЕ");
+    }
+    return result;
+}
+
+string WriteNubers(int n)
+{
+    if (n == 1) return ($"{n} ");
+    else
+    {
+        return n.ToString() + "," + WriteNubers(n - 1);
+    }
+}
+
+
+int n = GetNumber("Введите N -> ");
+Console.WriteLine(WriteNubers(n));
