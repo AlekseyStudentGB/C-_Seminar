7e00e16964904cc51e7b72a292d41b2b20350426
Mutniyh <wrobotw@mail.ru>
2023-02-27 09:39:45 +0300

    Решение задачи 2 к семинару 9

--- HW_9_2/Program.cs
+++ HW_9_2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+
+*/
+int GetNumber(string msg)
+{
+    Console.Write(msg);
+    int result = 0;
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result)) break;
+        else Console.WriteLine("НЕ КОРРЕКТНОЕ ЗНАЧЕНИЕ");
+    }
+    return result;
+}
+
+int WriteNubers(int m,int n)
+{
+    if (n == m) return (n);
+    else 
+    {
+        
+       return n + WriteNubers(m,n-1) ;
+    }
+}
+
+
+int m = GetNumber("Введите M -> ");
+int n = GetNumber("Введите N -> ");
+Console.WriteLine(WriteNubers(m,n));
